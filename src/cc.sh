d596c6aaa7e3c07a72bd47267119702db28ae838
Johnny Cai <jaihysc@gmail.com>
2022-05-26 23:52:34 -0400

    Improve cc.sh option handling
    - Rename codegen to asmgen in cc.sh, asmgen is the stage's correct
      name
    - Remove need for 2 dashes to specify options, less typing. e.g.,
      before was -P-Zd4, now is -PZd4. To pass multiple words, quote
      the option, e.g., "-Pdp gc" to pass -dp gc to the parser

--- src/cc.sh
+++ src/cc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Preprocessor, Parse, Codegen, Assembler, Linker
+# Preprocessor, Parse, Asmgen, Assembler, Linker
 pp_flags=()
 parse_flags=()
-cg_flags=()
+ag_flags=()
 asm_flags=()
 ln_flags=()
 
@@ -26,22 +26,22 @@ do
             echo "Prefixes"
             echo "-E prEprocessor"
             echo "-P Parser"
-            echo "-C Codegen"
+            echo "-S aSsembly generator"
             echo "-A Assembler"
             echo "-L Linker"
             echo "Pass the prefix to indicate the compilation stage to pass the flag to, followed by flag for the stage"
-            echo "Example: -E-E -E-x to pass the flags -E -x to the preprocessor"
+            echo "Example: -EE -Ex \"-PZd ef gh\" to pass the flags -E -x to the preprocessor and -Zd ef gh to the parser"
             exit 1
         elif [[ "$arg" =~ ^-E.* ]]; then
-            pp_flags+=("${arg:2}") # Discard the prefix
+            pp_flags+=("-${arg:2}") # Discard the prefix
         elif [[ "$arg" =~ ^-P.* ]]; then
-            parse_flags+=("${arg:2}")
-        elif [[ "$arg" =~ ^-C.* ]]; then
-            cg_flags+=("${arg:2}")
+            parse_flags+=("-${arg:2}")
+        elif [[ "$arg" =~ ^-S.* ]]; then
+            ag_flags+=("-${arg:2}")
         elif [[ "$arg" =~ ^-A.* ]]; then
-            asm_flags+=("${arg:2}")
+            asm_flags+=("-${arg:2}")
         elif [[ "$arg" =~ ^-L.* ]]; then
-            ln_flags+=("${arg:2}")
+            ln_flags+=("-${arg:2}")
         else
             echo "Unrecognized prefix $arg"
         fi
@@ -56,11 +56,11 @@ if [ ${#input_files[@]} -eq 0 ]; then
     exit 2
 elif [ ${#input_files[@]} -eq 1 ]; then
     file=${input_files[0]}
-    gcc -E -x c               "${pp_flags[@]}"    "$file"                   -o "$(dirname "$file")/imm1"  || fail "Preprocessor error"   5
-    "$(dirname "$0")/parse"   "${parse_flags[@]}" "$(dirname "$file")/imm1" -o "$(dirname "$file")/imm2"  || fail "Parser error"         6
-    "$(dirname "$0")/asm_gen" "${cg_flags[@]}"    "$(dirname "$file")/imm2" -o "$(dirname "$file")/imm3"  || fail "Code generator error" 7
-    nasm -felf64              "${asm_flags[@]}"   "$(dirname "$file")/imm3" -o "$(dirname "$file")/imm4"  || fail "Assembler error"      8
-    ld                        "${ln_flags[@]}"    "$(dirname "$file")/imm4" -o "$(dirname "$file")/a.out" || fail "Linker error"         9
+    gcc -E -x c               "${pp_flags[@]}"    "$file"                   -o "$(dirname "$file")/imm1"  || fail "Preprocessor error"       5
+    "$(dirname "$0")/parse"   "${parse_flags[@]}" "$(dirname "$file")/imm1" -o "$(dirname "$file")/imm2"  || fail "Parser error"             6
+    "$(dirname "$0")/asm_gen" "${ag_flags[@]}"    "$(dirname "$file")/imm2" -o "$(dirname "$file")/imm3"  || fail "Assembly generator error" 7
+    nasm -felf64              "${asm_flags[@]}"   "$(dirname "$file")/imm3" -o "$(dirname "$file")/imm4"  || fail "Assembler error"          8
+    ld                        "${ln_flags[@]}"    "$(dirname "$file")/imm4" -o "$(dirname "$file")/a.out" || fail "Linker error"             9
 else
     echo "Only 1 input file supported"
     exit 3
